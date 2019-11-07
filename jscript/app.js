bc7e9fb419b9e439ca9b6efb28ae2d743674d896
EsmarlynDiaz <Esmarlyndiaz23@gmail.com>
2019-11-07 15:59:31 -0400

    Add table

--- jscript/app.js
+++ jscript/app.js
@@ -8,15 +8,16 @@ var getData = function() {
 };
 
 function obtenerdatos() {
+
     // Estado de Resultado
     var ventasnetas = document.getElementById("ventasnetas").value;
     var costoventas = document.getElementById("costoventas").value;
-    utilidadbruta = ventasnetas - costoventas;
+    var utilidadbruta = parseInt(ventasnetas) - parseInt(costoventas);
 
     var gastosadmin = document.getElementById("gastosadmin").value;
     var gastosventas = document.getElementById("gastosventas").value;
     var depreciacion = document.getElementById("depreciacion").value;
-    utilidadoperativa = utilidadbruta - (gastosadmin + gastosventas + depreciacion);
+    var utilidadoperativa = parseInt(utilidadbruta) - (parseInt(gastosadmin) + parseInt(gastosventas) + parseInt(depreciacion));
 
     var gastosfinancieros = document.getElementById("gastosfinancieros").value;
     var utilidadantesimpuestosintereses = utilidadoperativa - gastosfinancieros;
@@ -27,10 +28,85 @@ function obtenerdatos() {
     var impuestos = document.getElementById("impuestos").value;
     var utilidadneta = utilidadantesimpuestos - impuestos;
 
-    console.log("variables");
+    // console.log();
+
     document.estadoresultado.utilidadbruta.value = utilidadbruta;
     document.estadoresultado.utilidadoperativa.value = utilidadoperativa;
     document.estadoresultado.utilidadantesimpuestosintereses.value = utilidadantesimpuestosintereses;
     document.estadoresultado.utilidadantesimpuestos.value = utilidadantesimpuestos;
     document.estadoresultado.utilidadneta.value = utilidadneta;
+
+
+    //Balance General
+    var efectivo = document.getElementById("efectivo").value;
+    var cuentasporcobrar = document.getElementById("cuentasporcobrar").value;
+    var inventario = document.getElementById("inventario").value;
+    var activoscirculantes = parseInt(efectivo) + parseInt(cuentasporcobrar) + parseInt(inventario);
+
+    var activosfijos = document.getElementById("activosfijos").value;
+    var totalactivos = parseInt(activoscirculantes) + parseInt(activosfijos);
+
+    var pasivoscirculantes = document.getElementById("pasivoscirculantes").value;
+    var pasivoslargoplazo = document.getElementById("pasivoslargoplazo").value;
+    var pasivostotales = parseInt(pasivoscirculantes) + parseInt(pasivoslargoplazo);
+
+
+    var capitalcomun = document.getElementById("capitalcomun").value;
+
+    var pasivomascapital = parseInt(pasivostotales) + parseInt(capitalcomun);
+
+
+    document.balancegeneral.activoscirculantes.value = activoscirculantes.toString(10);
+    document.balancegeneral.totalactivos.value = totalactivos;
+    document.balancegeneral.totalpasivos.value = pasivostotales;
+    // document.balancegeneral..value = ;
+}
+
+function calcularRazones() {
+    var ventasNetas = parseInt(document.getElementById("ventasnetas").value);
+    var EBIT = parseInt(document.getElementById("utilidadantesimpuestosintereses").value);
+    var intereses = parseInt(document.getElementById("intereses").value);
+    var ingresosNetos = parseInt(document.getElementById("utilidadneta").value);
+    var cuentasporcobrar = parseInt(document.getElementById("cuentasporcobrar").value);
+    var inventario = parseInt(document.getElementById("inventario").value);
+    var activoscirculantes = parseInt(document.getElementById("activoscirculantes").value);
+    var activosfijos = parseInt(document.getElementById("activosfijos").value);
+    var activostotales = parseInt(document.getElementById("totalactivos").value);
+    var pasivoscirculantes = parseInt(document.getElementById("pasivoscirculantes").value);
+    var pasivostotales = parseInt(document.getElementById("totalpasivos").value);
+    var capitalcomun = parseInt(document.getElementById("capitalcomun").value);
+
+    var R1 = activoscirculantes / pasivoscirculantes;
+    var R2 = (activoscirculantes - inventario) / pasivoscirculantes;
+    var R3 = ventasNetas / inventario;
+    var R4 = cuentasporcobrar / (ventasNetas / 360);
+    var R5 = ventasNetas / activosfijos;
+    var R6 = ventasNetas / activostotales;
+    var R7 = pasivostotales / activostotales;
+    var R8 = EBIT / intereses;
+    var R9 = ingresosNetos / ventasNetas;
+    var R10 = EBIT / activostotales;
+    var R11 = ingresosNetos / activostotales;
+    var R12 = ingresosNetos / capitalcomun;
+
+    var razones = [R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12];
+
+
+    for (ted = 0; ted < razones.length; ted++) {
+        document.getElementById('r' + (ted + 1)).innerHTML = razones[ted];
+        // element = razones[ted];
+    }
+
+}
+
+function comparacion() {
+
+    var razones = [];
+    var comparar = [];
+
+    for (ted = 0; ted < array.length; ted++) {
+        razones.push(parseInt(document.getElementById('r' + (ted + 1)).value));
+        comparar.push(parseInt(document.getElementById('estado' + (ted + 1)).value));
+    }
+    console.log(razones, comparar);
 }
